9edff663dc2ce497f17b83a94f8c312fbe64026c
zz35 <zz35@klovia.cs.st-andrews.ac.uk>
2018-03-28 00:40:51 +0100

    remove testing file

--- test.java
+++
@@ -1,7 +0,0 @@
-public class test {
-    public static void main(String args[]) {
-            System.out.println("Hello world");
-            System.out.println("testing saving");
-	    System.out.println("what is github"); //This is branch working
-    }
-}
